34113d7076065c7be2fcc7d9aba1e41227bd3399
jdial1996 <jdial1996@yahoo.co.uk>
2023-10-24 22:38:41 +0100

    add iam oidc role to push to ecr

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, Response
+import logging
 
 app = Flask(__name__)
 
 
+# Configure logging
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
+
+
 @app.route("/")
 def index():
-    return "Plaform app with Python Flask running on Kubernetes =)!"
+    message = "Plaform app with Python Flask running on Kubernetes =)!"
+    return Response(message, status=200, mimetype="application/json")
 
 
 if __name__ == "__main__":
+    logger.info("Flask application is running successfully")
     app.run(host="0.0.0.0", port=5000)
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,4 @@
 resource "aws_ecr_repository" "platform_app_ecr_repository" {
   name = "platform-app"
+  force_delete = true
 }
